d575b849cabe4ea87035460092f42453feb61b49
BasilDaboor <Basildaboor12@gmail.com>
2025-01-25 13:30:13 +0300

    make it responsive

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ var typed = new Typed(".web",{
     backSpeed:100,
     backDelay:1000,
     loop:true});
+
+
+
+    const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
